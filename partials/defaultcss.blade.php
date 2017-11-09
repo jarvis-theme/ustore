71e176cc22079c9d36a6605349ec69dbad2b379a
GYO\Jarvis <gistha.yogaswara>
2017-11-09 16:55:54 +0700

    update responsive image blog

--- partials/defaultcss.blade.php
+++ partials/defaultcss.blade.php
@@ -1,7 +1,7 @@
 	@if($tema->isiCss=='')
-	{{generate_theme_css('ustore/assets/css/stylesheet.css?v=001')}}  
+	{{generate_theme_css('ustore/assets/css/stylesheet.css?v=002')}}  
 	@else   
-	{{generate_theme_css('ustore/assets/css/editstylesheet.css?v=001')}}  
+	{{generate_theme_css('ustore/assets/css/editstylesheet.css?v=002')}}  
 	@endif  
 	{{generate_theme_css('ustore/assets/css/slideshow.css')}}  
 	{{generate_theme_css('ustore/assets/css/carousel.css')}}  
